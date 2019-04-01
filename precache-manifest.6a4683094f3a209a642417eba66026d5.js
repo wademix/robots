82e9cda6acf3702eccc9d95ad5d6cd3e913e68b8
Wade Oghenejabor <oghenejaborwade@gmail.com>
2019-04-01 13:19:06 +0100

    Updates

---
+++ precache-manifest.6a4683094f3a209a642417eba66026d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robots/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "840e4d37dfd0ea1eedc1",
+    "url": "/robots/static/js/runtime~main.3ed066c5.js"
+  },
+  {
+    "revision": "b2a184f4ff79c2697583",
+    "url": "/robots/static/js/main.8c5e0a01.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/robots/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "b2a184f4ff79c2697583",
+    "url": "/robots/static/css/main.9b174e41.chunk.css"
+  },
+  {
+    "revision": "c86d0b20af985eefb3d0e4172dd177f7",
+    "url": "/robots/index.html"
+  }
+];
